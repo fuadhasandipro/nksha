8366d3fc7c5fcfe54746d44fe6d26c95b8523415
Fuad Hasan <68866471+fuadhasandipro@users.noreply.github.com>
2024-06-21 22:08:08 +0600

    Update [[...index]].tsx

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
   return (
-    <div className="flex h-full items-center justify-center bg-white">
+    <div className="flex h-screen items-center justify-center bg-white">
       <SignIn />
     </div>
   );
